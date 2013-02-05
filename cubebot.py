1c80f6424cfcefb1073093371f68d445836bf4c5
Viraj Sinha <vsinha@purdue.edu>
2013-02-04 19:56:45 -0500

    responds to greetings with a semi-randomized greeting

--- cubebot.py
+++ cubebot.py
@@ -1,13 +1,10 @@
-import sys
-import logging
 import getpass
+import logging
 from optparse import OptionParser
+import operator
+import random
 import sleekxmpp
-
-#for weather
-import urllib3
-import pywapi
-import string
+import sys
 
 # ensure unicode is handled properly incase python is not v3.0
 if sys.version_info < (3, 0):
@@ -17,29 +14,49 @@ else:
 	raw_input = input
 
 
+greetingwords = ["greetings", "hey", "hi", "hello", "sup", "yo", "hi there"]
+greetingwords_set = set(greetingwords)
+
+swearwords = ["fuck", "suck"] #don't want cube being bullied
+swearwords_set = set(swearwords)
+
+
 class MUCBot (sleekxmpp.ClientXMPP):
 
 	def __init__ (self, jid, password, room, nick):
 		sleekxmpp.ClientXMPP.__init__(self, jid, password)
-
 		self.room = room
 		self.nick = nick
-		
-		self.add_event_handler("session_start", self.start)
-		self.add_event_handler("groupchat_message", self.bot_message)
+		self.add_event_handler("session_start", self.start) #begin receiving/responding
+		self.add_event_handler("groupchat_message", self.bot_message)  
 	
 	def start(self, event):
 		self.get_roster()
 		self.send_presence()
-		self.plugin['xep_0045'].joinMUC(self.room, self.nick)
+		self.plugin['xep_0045'].joinMUC(self.room, self.nick) #enable group chat
+
 
 	def bot_message(self, msg):
 		#reply if username is mentioned
 		#always make sure the message we're replying to didn't come from self
-		if msg['mucnick'] != self.nick and self.nick in msg['body']:
-			self.send_message(mto=msg['from'].bare,
-					mbody="I heard that, %s." % msg['mucnick'], mtype='groupchat')
 
+		#convert all input to lowercase
+		message_body = [x.lower() for x in msg['body'].split()] 
+		human_nick = msg['mucnick'] # whoever we're responding to
+
+		if human_nick != self.nick and self.nick in message_body:
+
+			if any( word in greetingwords_set for word in message_body ):
+				response = random.choice(greetingwords) + ", " + human_nick
+
+			if any( word in swearwords_set for word in message_body):
+				response = human_nick + ", your mother is a whore"
+
+			self.send_message(mto=msg['from'].bare, mbody=response, mtype='groupchat')
+
+# TODO
+# fuck off cube
+# <user>, your <noun> is a <noun>
 
 
 if __name__ == '__main__':
@@ -63,9 +80,7 @@ if __name__ == '__main__':
     logging.basicConfig(level=opts.loglevel,
                         format='%(levelname)-8s %(message)s')
 
-    # Setup the MUCBot and register plugins. Note that while plugins may
-    # have interdependencies, the order in which you register them does
-    # not matter.
+    # Setup the MUCBot and register plugins.
     xmpp = MUCBot("cube@obnauticus.com", "chickentender23", "#emc2@conference.obnauticus.com", "cube")
     xmpp.register_plugin('xep_0030') # Service Discovery
     xmpp.register_plugin('xep_0045') # Multi-User Chat
@@ -73,13 +88,6 @@ if __name__ == '__main__':
 
     # Connect to the XMPP server and start processing XMPP stanzas.
     if xmpp.connect(('obnauticus.com', 5222)):
-        # If you do not have the dnspython library installed, you will need
-        # to manually specify the name of the server if it does not match
-        # the one in the JID. For example, to use Google Talk you would
-        # need to use:
-        #
-        # if xmpp.connect(('talk.google.com', 5222)):
-        #     ...
         xmpp.process(block=True)
         print("Done")
     else:
