ae67dcef893a1092fd951167448de567063c167c
synha <vsinha@purdue.edu>
2013-05-23 16:06:59 -0400

    will be used to store a list of recently said words, to pick from for more contextual speech

---
+++ wordpool.py
@@ -0,0 +1,38 @@
+class WordPool (object):
+	#we're keeping a list of recently used words
+
+	def __init__(self, poolsize):
+		self.poolsize = poolsize
+		self.i = 0
+		self.pool = [] 
+
+	def addSentence(self, sentence):
+		for word in sentence:
+			print("adding: " + word)
+			if len(self.pool) < self.poolsize:
+				#fill the list until it's at the right size
+				self.pool.append(word)
+			else:
+				self.pool[self.i] = word
+				self.i += 1
+				if self.i >= self.poolsize:
+					self.i = 0
+	
+
+if __name__ == "__main__":
+	recent = WordPool(4)
+	sentence = "this is four words"
+	sentence = sentence.split()
+
+	recent.addSentence(sentence)
+	print(recent.pool)
+	recent.addSentence("1".split())
+	print(recent.pool)
+	recent.addSentence("2".split())
+	print(recent.pool)
+	recent.addSentence("3".split())
+	print(recent.pool)
+	recent.addSentence("4".split())
+	print(recent.pool)
+	recent.addSentence("test".split())
+	print(recent.pool)
