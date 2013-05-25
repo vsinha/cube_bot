eb1f677d7cf0226cb2d8afc477e762057afc2cb6
synha <vsinha@purdue.edu>
2013-05-24 22:31:50 -0400

    commit for ian

--- markov.py
+++ markov.py
@@ -1,7 +1,6 @@
 import logging
 import random
 
-from itertools import chain
 from bulbs.neo4jserver import Graph
 from graph import Key, Link
 from wordpool import WordPool
@@ -40,12 +39,6 @@ class Markov (object):
 		self.words = self.sentenceToWords(sentence)
 		self.storeToDatabase()
 
-		#save the cache every n messages
-		n = 20
-		self.counter += 1
-		if self.counter % n == 0:
-			self.counter = 0 #reset the counter
-
 	def tripletGenerator(self):
 		#starts at the first word and goes forward
 		if len(self.words) < 3:
@@ -57,40 +50,55 @@ class Markov (object):
 			yield triplet
 
 	def storeToDatabase(self):
-		last = ''
-		lastVertex = self.g.vertices.create(data = last)
+		#initialize with empty string, which we know already exists in the db
+		last = '__ROOT__' 
+		lastVertex = self.g.vertices.index.lookup(data = last)
+		#^this has to return a list of size 1, every time
+		print("lastVertex: ", lastVertex)
 
 		#store triplet, and connect with previously stored one
 		for triplet in self.tripletGenerator():
 			current = ' '.join(triplet) #saving entries as strings (for now)
-			print(current)
+			print("last: ", last)
+			print("current: ", current)
+			#if our previous string exists already, we need to append
+			lookup = self.g.vertices.index.lookup(data=last) #returns generator
+			try:
+				lookup = list(lookup) #convert generator to list
+			except TypeError: #lookup is empty
+				pass
+
+			print("lookup: ", lookup)
 			currentVertex = self.g.vertices.create(data = current)
-			self.g.edges.create(lastVertex, "link", currentVertex)
+
+			if len(lookup) != 0: #this exists already
+				#so link with results of lookup
+				self.g.edges.create(lookup[0], "link", currentVertex)
+			else:
+				#otherwise, link with whatever we just created
+				self.g.edges.create(lastVertex, "link", currentVertex)
+			last = current
 			lastVertex = currentVertex
 
 	def generateText(self):
 		#pick a random vertex
-		print(len(self.g.V))
-		randnum = random.randint(len(self.g.V))
-		print(randnum)
+		randnum = random.randint(1, len(self.g.V))
 		seed = self.g.vertices.get(randnum)
 		print(seed)
 
-
-	def clearDB(self):
+	def resetDB(self):
 		self.g.clear()
+		self.g.vertices.create(data = '__ROOT__') #initialize w/ empty string node
 
 #for testing
 if __name__ == '__main__':
 	m = Markov()
-	m.clearDB()
+	m.resetDB()
 	sentences = []
 	sentences.append("cube is a program")
-	"""
 	sentences.append("cube is a robot")
 	sentences.append("cube is not a truck")
 	sentences.append("a truck has wheels")
-	"""
 	for sentence in sentences:
 		m.addNewSentence(sentence.split())
 	m.generateText()
