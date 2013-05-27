5ceb971f28d2c30ec1772dbb9a97d7fab1a0ae5b
Ian Whitlock <ian@theknown.net>
2013-05-27 02:45:07 -0400

    neo4j stuff in progress, getting tired, figured i should start making commits

--- logparser.py
+++ logparser.py
@@ -1,10 +1,12 @@
-import os, re
+import os
+from bulbs.neo4jserver import Graph
+from graph import Key, Link
 
 class LogParser():
 
-
-	def __init__ (self):
-		self.numlines = 0
+	def __init__(self):
+		g = Graph()
+		self.sentences = []
 
 	def stripMetadata(self, line):
 		divider = line.find(": ")
@@ -12,12 +14,20 @@ class LogParser():
 			return ""
 		return line[divider+2:]
 
+	def buildGraph(self):
+		root = g.vertices.create(token="__END__")
+		
+
 
 	def parse(self, file):
 		f = open(file, 'r')
 		for line in f.readlines():
 			line = self.stripMetadata(line[0:-1])
-			self.numlines += 1
+			if line == "":
+				continue
+			words = line.split(" ")
+			self.sentences.append(words)
+		buildGraph(sentences)
 
 	def get_numlines(self):
 		return self.numlines
@@ -27,4 +37,3 @@ if __name__ == '__main__':
 	for root, dirs, files in os.walk("logs"):
 		for file in files:
 			lp.parse("logs/"+file)
-	print(lp.get_numlines())
